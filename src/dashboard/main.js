6f08d5ab6b243ac5339153388e1920e80e404fd1
guaskeila18 <guaskeila18@gmail.com>
2023-12-24 01:34:32 -0300

    navbar

--- src/dashboard/main.js
+++ src/dashboard/main.js
@@ -3,3 +3,6 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+import imgUrl from '../../public/img/secundario.png'
+document.getElementById('secundario').src = imgUrl
